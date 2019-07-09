0146e3d8023e9301afb8bd8e1fd4a16405a4b0e3
mylovetmcg <mylovetmcg@vip.qq.com>
2019-07-09 15:59:22 +0800

    swiper 修改成功

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import router from './router'
 import fastClick from 'fastclick'
 import '@/assets/styles/reset.css'
 import '@/assets/styles/border.css'
 import '@/assets/styles/iconfont.css'
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 fastClick.attach(document.body)
 
 /* eslint-disable no-new */
---
+++ Travel/src/pages/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-image" :src="item.imgUrl" />
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop: true
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/a4c27b0a5fc1d9e9e620225869089b12.jpg_750x200_e4720428.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg"
+        }
+      ],
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #ffffff !important;
+}
+
+.wrapper {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  padding-bottom: 25%;
+  background-color: #eee;
+
+  .swiper-image {
+    width: 100%;
+  }
+}
+</style>
+
